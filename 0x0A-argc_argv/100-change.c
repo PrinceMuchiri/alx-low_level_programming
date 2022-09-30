e522e6450af7043db01f071afa807350f5572040
PrinceMuchiri <110984211+PrinceMuchiri@users.noreply.github.com>
2022-09-30 17:40:47 +0300

    advanced task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,72 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
-{
-	int count = 0;
-
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-			else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
- */
+* main - Program give the amount of cents.
+* @argc: Number of command lines arguments.
+* @argv: Character of arrays.
+*
+* Return: 0 for success or 1 for error.
+*/
 int main(int argc, char *argv[])
 {
-	int i, coin;
+	int i;
+	unsigned int cents[5] = {25, 10, 5, 2, 1};
+	unsigned long int num = 0;
+	unsigned long int n_cents[5] = {0};
+	unsigned long int result = 0;
 
-	coin = 0;
-
-	if (argc != 2)
+	if (argc == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
-		printf("0\n");
-
-	else
+	if (argv[1][0] == '-')
 	{
-		coin = coinConverter(i);
-
-		printf("%d\n", coin);
+		printf("0\n");
+		return (0);
 	}
-
+	if (argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num = atoi(argv[1]);
+	for (i = 0; i <= 4; i++)
+	{
+		n_cents[i] = num / cents[i];
+		num = num % cents[i];
+	}
+	result = n_cents[0] + n_cents[1] + n_cents[2] + n_cents[3] + n_cents[4];
+	printf("%lu\n", result);
 	return (0);
 }
